0cb3a21ba50bd63fb25c216eaba68cf4130f0cc6
Brad Miller <bonelake@mac.com>
2017-02-09 16:35:11 -0600

    initial commit

---
+++ pavement.py
@@ -0,0 +1,46 @@
+import paver
+from paver.easy import *
+import paver.setuputils
+paver.setuputils.install_distutils_tasks()
+import os, sys
+from runestone.server import get_dburl
+from sphinxcontrib import paverutils
+import pkg_resources
+
+sys.path.append(os.getcwd())
+
+home_dir = os.getcwd()
+master_url = 'http://127.0.0.1:8000'
+master_app = 'runestone'
+serving_dir = "./build/sigcse2017"
+dest = "../../static"
+
+options(
+    sphinx = Bunch(docroot=".",),
+
+    build = Bunch(
+        builddir="./build/sigcse2017",
+        sourcedir="_sources",
+        outdir="./build/sigcse2017",
+        confdir=".",
+        project_name = "sigcse2017",
+        template_args={'course_id': 'sigcse2017',
+                       'login_required':'false',
+                       'appname':master_app,
+                       'loglevel': 0,
+                       'course_url':master_url,
+                       'use_services': 'false',
+                       'python3': 'Ebookstrue',
+                       'dburl': '',
+                       'basecourse': 'sigcse2017'
+                        }
+    )
+)
+
+version = pkg_resources.require("runestone")[0].version
+options.build.template_args['runestone_version'] = version
+
+# If DBUSER etc. are in the environment override dburl
+options.build.template_args['dburl'] = get_dburl(outer=locals())
+
+from runestone import build  # build is called implicitly by the paver driver.
